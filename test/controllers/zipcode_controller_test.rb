69e36fb3754d4d5fa623c4eb3ec007686f7313c7
Rodolfo Valdés <rovalcor@gmail.com>
2018-07-30 14:36:27 -0500

    Set up route

--- test/controllers/zipcode_controller_test.rb
+++ test/controllers/zipcode_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
-class ZipcodesControllerTest < ActionController::TestCase
+class ZipcodeControllerTest < ActionController::TestCase
 
   def test_show_weather_by_zipcode
     VCR.use_cassette('google_to_darksky_weather_by_zipcode') do
-      get :show, params: { id: 72474 }
+      get :show, params: { id: 10018 }
       assert_response :success
     end
   end
