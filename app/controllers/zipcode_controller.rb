954919d8af46b97138aec4ed9fa8835835aa009c
Rodolfo Valdés <rovalcor@gmail.com>
2018-07-30 16:20:40 -0500

    Add template when zipcode is not found

--- app/controllers/zipcode_controller.rb
+++ app/controllers/zipcode_controller.rb
@@ -2,9 +2,14 @@ class ZipcodeController < ApplicationController
 
   def show
     latlng = GoogleMaps::ZipcodeToGeocodeConverter.convert(params[:id])
-    temperatures = DarkSky::TemperatureRequestor.get_temperatures_for_today(latlng)
-    @current_temperature = temperatures[:current]
-    @high_temperature = temperatures[:high]
-    @low_temperature = temperatures[:low]
+
+    if latlng.present?
+      temperatures = DarkSky::TemperatureRequestor.get_temperatures_for_today(latlng)
+      @current_temperature = temperatures[:current]
+      @high_temperature = temperatures[:high]
+      @low_temperature = temperatures[:low]
+    else
+      render template: 'zipcode/not_found'
+    end
   end
 end
